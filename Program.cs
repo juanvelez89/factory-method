9c940c651e37abfdff38b751cdf5d4ca6162e14c
jdaniel.vg@aguapanelab.co <juan.velezw@gmail.com>
2020-08-21 09:08:36 -0500

    primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace factory_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
